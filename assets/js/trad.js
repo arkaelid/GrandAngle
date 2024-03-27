084ca419d5263eaa97ea97911fb794fa72be512e
sullivan jarry <sullivan.jarry@orange.fr>
2024-03-27 07:23:15 +0100

    TRAD

--- assets/js/trad.js
+++ assets/js/trad.js
@@ -1,22 +1,10 @@
   
-/*   link.forEach(el => {
-    el.addEventListener('click', () => {
-     langEl.querySelector('.active').classList.remove('active');
-     el.classList.add('active');})
-    });
-     */
-/*   function changeLang() {
-    document.getElementById('form_lang').submit();
-}
- */
-/* function selectLang() {
-    document.getElementById('select_lang').submit();
-} */
+
 
 function changeLang(lang) {
-    // Mettre à jour l'URL avec la nouvelle langue sélectionnée
-    var currentUrl = window.location.href;
-    var newUrl;
+    
+    let currentUrl = window.location.href;
+    let newUrl;
 
     if (currentUrl.indexOf('?') !== -1) {
         newUrl = currentUrl.replace(/lang=\w\w/g, 'lang=' + lang);
@@ -24,6 +12,5 @@ function changeLang(lang) {
         newUrl = currentUrl + '?lang=' + lang;
     }
 
-    // Recharger la page avec la nouvelle URL
     window.location.href = newUrl;
 }
